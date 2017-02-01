076785db5794e320a2d97e36d0912e24c5882755
Chris Dusyk <chris.dusyk@gmail.com>
2017-02-01 10:25:14 -0600

    Format spaces to tabs

--- src/XdtTransform.Tests/XdtTransformerTests.cs
+++ src/XdtTransform.Tests/XdtTransformerTests.cs
@@ -4,81 +4,82 @@ using XdtTransform.Impl;
 
 namespace XdtTransform.Tests
 {
-    [TestClass]
-    public class XdtTransformerTests
-    {
-        const String GenericSourceXml = @"<?xml version=""1.0""?>
+	[TestClass]
+	public class XdtTransformerTests
+	{
+		private const String GenericSourceXml = @"<?xml version=""1.0""?>
 <configuration>
 </configuration>";
-        const String GenericTransformXml = @"<?xml version=""1.0""?>
+
+		private const String GenericTransformXml = @"<?xml version=""1.0""?>
 <configuration xmlns:xdt=""http://schemas.microsoft.com/XML-Document-Transform"">
 </configuration>";
 
-        #region Parameter Checking
+		#region Parameter Checking
 
-        [TestCategory("Parameter Checking"), TestMethod]
-        [ExpectedException(typeof(ArgumentNullException))]
-        public void Should_not_accept_a_null_source_parameter()
-        {
-            // Arrange
+		[TestCategory("Parameter Checking"), TestMethod]
+		[ExpectedException(typeof(ArgumentNullException))]
+		public void Should_not_accept_a_null_source_parameter()
+		{
+			// Arrange
 
-            // Act
+			// Act
 
-            // Assert
-            XdtTransformer.Current.Transform(null, GenericTransformXml);
-        }
+			// Assert
+			XdtTransformer.Current.Transform(null, GenericTransformXml);
+		}
 
-        [TestCategory("Parameter Checking"), TestMethod]
-        [ExpectedException(typeof(ArgumentNullException))]
-        public void Should_not_accept_an_empty_source_parameter()
-        {
-            // Arrange
+		[TestCategory("Parameter Checking"), TestMethod]
+		[ExpectedException(typeof(ArgumentNullException))]
+		public void Should_not_accept_an_empty_source_parameter()
+		{
+			// Arrange
 
-            // Act
+			// Act
 
-            // Assert
-            XdtTransformer.Current.Transform(String.Empty, GenericTransformXml);
-        }
+			// Assert
+			XdtTransformer.Current.Transform(String.Empty, GenericTransformXml);
+		}
 
-        [TestCategory("Parameter Checking"), TestMethod]
-        [ExpectedException(typeof(ArgumentNullException))]
-        public void Should_not_accept_a_null_transform_parameter()
-        {
-            // Arrange
+		[TestCategory("Parameter Checking"), TestMethod]
+		[ExpectedException(typeof(ArgumentNullException))]
+		public void Should_not_accept_a_null_transform_parameter()
+		{
+			// Arrange
 
-            // Act
+			// Act
 
-            // Assert
-            XdtTransformer.Current.Transform(GenericSourceXml, null);
-        }
+			// Assert
+			XdtTransformer.Current.Transform(GenericSourceXml, null);
+		}
 
-        [TestCategory("Parameter Checking"), TestMethod]
-        [ExpectedException(typeof(ArgumentNullException))]
-        public void Should_not_accept_an_empty_transform_parameter()
-        {
-            XdtTransformer.Current.Transform(GenericSourceXml, String.Empty);
-        }
+		[TestCategory("Parameter Checking"), TestMethod]
+		[ExpectedException(typeof(ArgumentNullException))]
+		public void Should_not_accept_an_empty_transform_parameter()
+		{
+			XdtTransformer.Current.Transform(GenericSourceXml, String.Empty);
+		}
 
-        #endregion
+		#endregion Parameter Checking
 
-        #region Simple Usage
+		#region Simple Usage
 
-        [TestCategory("Simple Usage"), TestMethod]
-        public void Should_handle_simple_transform()
-        {
-            // Arrange
-            var source = GenericSourceXml;
-            var transform = GenericTransformXml;
-            var expected = GenericSourceXml;
+		[TestCategory("Simple Usage"), TestMethod]
+		public void Should_handle_simple_transform()
+		{
+			// Arrange
+			var source = GenericSourceXml;
+			var transform = GenericTransformXml;
+			var expected = GenericSourceXml;
 
-            // Act
-            var actual = XdtTransformer.Current.Transform(source, transform);
+			// Act
+			var actual = XdtTransformer.Current.Transform(source, transform);
 
-            // Assert
-            Assert.IsTrue(!String.IsNullOrEmpty(actual), "Should not return null or empty value.");
-            Assert.AreEqual(expected, actual, "Actual did not match expected result.");
-        }
+			// Assert
+			Assert.IsTrue(!String.IsNullOrEmpty(actual), "Should not return null or empty value.");
+			Assert.AreEqual(expected, actual, "Actual did not match expected result.");
+		}
 
-        #endregion
-    }
+		#endregion Simple Usage
+	}
 }
--- src/XdtTransform/Impl/XdtTransformException.cs
+++ src/XdtTransform/Impl/XdtTransformException.cs
@@ -3,23 +3,26 @@ using System.Runtime.Serialization;
 
 namespace XdtTransform.Impl
 {
-    [Serializable]
-    public class XdtTransformException : Exception
-    {
-        public XdtTransformException()
-        {
-        }
-        public XdtTransformException(String message)
-            : base(message)
-        {
-        }
-        public XdtTransformException(String message, Exception inner)
-            : base(message, inner)
-        {
-        }
-        protected XdtTransformException(SerializationInfo info, StreamingContext context)
-            : base(info, context)
-        {
-        }
-    }
+	[Serializable]
+	public class XdtTransformException : Exception
+	{
+		public XdtTransformException()
+		{
+		}
+
+		public XdtTransformException(String message)
+			: base(message)
+		{
+		}
+
+		public XdtTransformException(String message, Exception inner)
+			: base(message, inner)
+		{
+		}
+
+		protected XdtTransformException(SerializationInfo info, StreamingContext context)
+			: base(info, context)
+		{
+		}
+	}
 }
